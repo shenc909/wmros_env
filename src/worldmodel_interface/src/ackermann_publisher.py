1ca59a2ff6eddbfd9efd3cde8085341137f3f436
Shen Chen <shenc909@users.noreply.github.com>
2020-11-30 21:12:36 +0800

    script to start and generate rosbag files for analysis later

--- src/worldmodel_interface/src/ackermann_publisher.py
+++ src/worldmodel_interface/src/ackermann_publisher.py
@@ -6,7 +6,9 @@ import math
 from ackermann_msgs.msg import AckermannDrive
 
 pub_rate = 30
-change_rate = 0.5
+change_rate = 0.2
+speed_multiplier = 0.7
+angle_multiplier = 0.5
 
 pub = rospy.Publisher('/ackermann_cmd', AckermannDrive, queue_size=10)
 rospy.init_node('ackermann_commander')
@@ -18,8 +20,8 @@ while not rospy.is_shutdown():
     change = pub_rate/change_rate
     msg = AckermannDrive()
     if i%change == 0:
-        steering_angle = (random.random() - 0.5) * 0.8 * math.pi
-        speed = random.random() * 1
+        steering_angle = (random.random() - 0.5) * angle_multiplier * math.pi
+        speed = random.random() * speed_multiplier
     msg.steering_angle = steering_angle
     msg.speed = speed
     pub.publish(msg)
---
+++ src/worldmodel_interface/src/generate_rosbags.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import rosgraph
+import roslaunch
+from rosbag_record import RosbagRecord
+import rospy
+
+track_prefix = 'track'
+track_number = 2
+
+rosbag_command = 'rosbag record -a --duration=30 --lz4'
+record_dir = '/home/shenc/recorded'
+
+def main():
+
+    # launch = roslaunch.scriptapi.ROSLaunch()
+    # launch.start()
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+
+    for track_num in range(1, track_number+1):
+        track_name = track_prefix + str(track_num)
+        rospy.logdebug("starting {}".format(track_name))
+        # args = 'world_name:={} gazebo_gui:=false rviz:=false'.format(track_name)
+        
+        spawn_map_args = ['worldmodel_interface','spawn_map_robot.launch','world_name:='+track_name, 'gazebo_gui:=false','rviz:=true']
+        
+        random_ctrl_args = ['worldmodel_interface', 'start_random_control.launch']
+        
+        spawn_map_launch_file = roslaunch.rlutil.resolve_launch_arguments(spawn_map_args)[0]
+        spawn_map_launch_args = spawn_map_args[2:]
+
+        random_ctrl_launch_file = roslaunch.rlutil.resolve_launch_arguments(random_ctrl_args)[0]
+
+        roslaunch_files = [(spawn_map_launch_file, spawn_map_launch_args), (random_ctrl_launch_file)]
+        launch = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_files)
+
+        launch.start()
+        command = rosbag_command + ' -o {}'.format(track_name)
+        RosbagRecord(command, record_dir)
+        rospy.sleep(35)
+        # rospy.spin()
+        launch.shutdown()
+
+        while rosgraph.is_master_online():
+            rospy.logerr_throttle_identical("ROSMASTER still running")
+    
+    # launch.shutdown()
+
+
+
+if __name__ == "__main__":
+    main()
